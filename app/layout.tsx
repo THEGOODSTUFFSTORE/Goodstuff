943b6458ccfbbe90f02c4f544f26948bf19aebc8
richardagaya <richardagaya278@gmail.com>
2025-09-12 16:04:50 +0300

    v133

--- app/layout.tsx
+++ app/layout.tsx
@@ -151,7 +151,7 @@ export default function RootLayout({
         <Script id="ga-gtag" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
+            function gtag(){dataLayer.push(arguments);} 
             gtag('js', new Date());
 
             gtag('config', 'G-65BEX1ZEVP');
